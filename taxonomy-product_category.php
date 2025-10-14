77bc9b10ab6acd149d61049ff33e26e066c32510
Amatiran179 <angga26442@gmail.com>
2025-10-14 09:17:06 +0700

    Create taxonomy-product_category.php

---
+++ taxonomy-product_category.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Product Category Taxonomy Template
+ * 
+ * @package PutraFiber
+ * @since 1.0.0
+ */
+
+// Gunakan archive-product.php sebagai template
+get_template_part('archive', 'product');
